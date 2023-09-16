6b16a60e8a8a01a342d74f3b9217252465af8de9
Anatoliy <anatoliy.darma@gmail.com>
2023-09-16 09:45:21 +0530

    Update ImageResizeComponent.vue

--- src/ImageResizeComponent.vue
+++ src/ImageResizeComponent.vue
@@ -55,7 +55,7 @@ function removeEventListener() {
 </script>
 
 <style lang="scss">
-##{$editorContainerId} {
+#{$editorContainerId} {
   .resize-trigger {
     display: none;
     position: absolute;
